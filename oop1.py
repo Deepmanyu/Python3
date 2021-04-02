ec0c52519090885168ababae15a2dd076a7d75f6
Deep Suhag / Deepmanyu Suhag <Deepmanyu.suhag@gmail.com>
2021-04-01 23:29:39 -0400

    Add files via upload

--- oop1.py
+++ oop1.py
@@ -26,7 +26,7 @@ class User:
 
 	@classmethod
 	def from_string(cls, data_str):
-		first, last, age = data_str.split(",")
+		first, last, age = data_str.split(",") 
 		return cls(first, last, int(age))
 
 	def __init__(self, first, last, age):
